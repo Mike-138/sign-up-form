297760d333bc909f91d29d671ab7e139984d1ca3
Michael <mikemcd.bbs@gmail.com>
2023-09-16 23:47:19 -0400

    Add password checking functionality

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const submitButton = document.querySelector("button");
 
+let givenPassword = document.querySelector("#password");
+let givenPasswordConfirm = document.querySelector("#confirm-pass");
+
+function callPasswordError(password, passwordConfirm) {
+    password.value = "";
+    passwordConfirm.value = "";
+    password.classList.add("error");
+    password.parentElement.classList.add("error-message");
+    passwordConfirm.classList.add("error");
+}
+
+givenPassword.addEventListener("keypress", () => {
+    if (givenPassword.classList.contains("error")) {
+        givenPassword.classList.remove("error");
+        givenPassword.parentElement.classList.remove("error-message");
+        givenPasswordConfirm.classList.remove("error");
+    }
+})
+
 submitButton.addEventListener("click", () => {
-    const givenPasswords = document.querySelectorAll("input[type='password']");
-    console.log(givenPasswords);
+    givenPassword = document.querySelector("#password");
+    givenPasswordConfirm = document.querySelector("#confirm-pass");
+    if (givenPassword.value !== givenPasswordConfirm.value) {
+        callPasswordError(givenPassword, givenPasswordConfirm);
+    }
 });
